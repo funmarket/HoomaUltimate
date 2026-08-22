8e843a40f421931470135ecabb369eb3dc652cf7
Out Of Sight <33332777+funmarket@users.noreply.github.com>
2026-08-22 20:50:21 +0100

    style: load Web account presentation

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { WebRouter } from "./app/router/WebRouter";
 import { initializeWebAppearance } from "./settings/theme";
 import "./styles.css";
+import "./account/account.css";
 
 initializeWebAppearance();
 
