926cea94dbf66a40b02ed5a837ad6f0f8d1e058c
Out Of Sight <33332777+funmarket@users.noreply.github.com>
2026-08-22 20:52:13 +0100

    style: load Telegram account presentation

--- apps/telegram/src/main.tsx
+++ apps/telegram/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { TelegramApp } from "./app/TelegramApp";
 import "./styles.css";
+import "./account/account.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
