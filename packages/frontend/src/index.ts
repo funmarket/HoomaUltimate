ee26bb197c1913948e322a41902f9d1b624e9f70
Out Of Sight <33332777+funmarket@users.noreply.github.com>
2026-09-12 21:04:16 +0100

    refactor(athletes): wire split calendar styles

--- packages/frontend/src/index.ts
+++ packages/frontend/src/index.ts
@@ -12,6 +12,8 @@ import "./athletes/athletes-controls.css";
 import "./athletes/athletes-member-management.css";
 import "./athletes/active-athletes.css";
 import "./athletes/calendar/athletes-calendar.css";
+import "./athletes/calendar/athletes-calendar-grid.css";
+import "./athletes/calendar/athletes-calendar-agenda.css";
 import "./athletes/calendar/athletes-calendar-form.css";
 import "./forms/hooma-form.css";
 import "./game-location/game-location.css";
